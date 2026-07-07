3e6a663fe5262a8bb3d0c1e9530d3e1629c0f2da
Natthanon Tahae <tahaenatthanon@gmail.com>
2026-07-07 12:48:49 +0700

    fix: add authorization params to GoogleProvider for account chooser

--- lib/auth.ts
+++ lib/auth.ts
@@ -28,6 +28,11 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
     }),
     CredentialsProvider({
       id: "credentials",
